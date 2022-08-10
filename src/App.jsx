7ca9ee2bc76262f87991e8cecaba08facf5fe8b1
kaitosun <110577892+kaitosun@users.noreply.github.com>
2022-08-10 17:00:13 +0900

    再レンダリングと副作用を知るの途中まで

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,43 @@
-import React from "react";
+/* eslint react-hooks/exhaustive-deps: off */
+import React, { useEffect, useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
-  const onClickButton = () => alert();
+  console.log("さいしょ");
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  };
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShowFlag || setFaceShowFlag(true);
+      } else {
+        faceShowFlag && setFaceShowFlag(false);
+      }
+    }
+  }, [num]);
+
   return (
     <>
-      <h1 style={{ color: "red" }}>こんにちは！</h1>
-      <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
-      <ColorfulMessage color="pink">元気です！</ColorfulMessage>
-      <button onClick={onClickButton}>ボタン</button>
+      <h1 style={{ color: "red" }}>世界の〇ベアツ</h1>
+      <ColorfulMessage color="blue">一世を風靡した名ギャグ</ColorfulMessage>
+      <ColorfulMessage color="orange">
+        今から数字を数えて、3の倍数の時にアホな顔になります。
+      </ColorfulMessage>
+      <button onClick={onClickCountUp}>カウントアップ！</button>
+      <br />
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
+      <p>{num}</p>
+      {/* faceShowFlagがfalseなら右辺を返す
+      {(faceShowFlag && num !== 0) || <p>(-ω-)</p>} */}
+      {/* faceShowFlagがtrueなら右辺を返す */}
+      {faceShowFlag && <p>(´◉◞౪◟◉)＜アホやで！</p>}
     </>
   );
 };
